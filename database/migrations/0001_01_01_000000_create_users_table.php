18ff397dff213943df47900838ed7f53ba4f2a32
mahdirnma <rahnamamahdi78@gmail.com>
2025-10-06 21:10:55 +0330

    fixed some problems

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role');
+            $table->boolean('is_active')->default(true);
+
             $table->rememberToken();
             $table->timestamps();
         });
