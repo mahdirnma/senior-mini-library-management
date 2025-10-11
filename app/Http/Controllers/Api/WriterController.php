1cdd7de00128d4f3f562631f52062d354583ecfb
mahdirnma <rahnamamahdi78@gmail.com>
2025-10-11 19:47:07 +0330

    create necessary files

---
+++ app/Http/Controllers/Api/WriterController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Writer;
+use Illuminate\Http\Request;
+
+class WriterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Writer $writer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Writer $writer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Writer $writer)
+    {
+        //
+    }
+}
